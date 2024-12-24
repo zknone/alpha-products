3761e2e027442155083e807a92f5baa4281b7645
Streltsov Ivan <principdereva@gmail.com>
2024-12-24 11:13:18 +0100

    feat: moved adapter

--- src/pages/products/products.tsx
+++ src/pages/products/products.tsx
@@ -1,31 +1,12 @@
 import { useEffect, useState } from "react";
 import fetchData from "../../utils/fetchData";
-import { Artwork, RawApiResponse, RawArtwork } from "../../type/type";
+import { Artwork, RawApiResponse } from "../../type/type";
+import { adaptArtwork } from "../../utils/artworkAdapter";
 
 const Products = () => {
   const [photos, setPhotos] = useState<Artwork[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-
-  function adaptArtwork(rawData: RawArtwork): Artwork {
-    return {
-      artistDisplay: rawData.artist_display,
-      categoryTitles: rawData.category_titles || [],
-      classificationTitles: rawData.classification_titles || [],
-      dateDisplay: rawData.date_display,
-      description: rawData.description,
-      id: rawData.id,
-      imageId: rawData.image_id,
-      mainReferenceNumber: rawData.main_reference_number,
-      thumbnail: {
-        lqip: rawData.thumbnail.lqip,
-        width: rawData.thumbnail.width,
-        height: rawData.thumbnail.height,
-        altText: rawData.thumbnail.alt_text,
-      },
-      title: rawData.title,
-    };
-  }
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,24 +33,7 @@ const Products = () => {
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
-      <ol>
-        {/* {photos.map(
-          (item) =>
-            item.primaryImageSmall && (
-              <li key={item?.objectID}>
-                <img
-                  width={300}
-                  height={300}
-                  src={item.primaryImageSmall}
-                ></img>
-                <span>{item?.artistDisplayName}</span>
-                <span>{item?.objectName}</span>
-                <span>{item?.objectDate}</span>
-                <span>{item?.culture}</span>
-              </li>
-            )
-        )} */}
-      </ol>
+      <ol></ol>
     </>
   );
 };
